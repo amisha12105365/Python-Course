7ed354ff39a2d9d2f13698af17d4821346da4865
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:51:04 +0530

    swap.py

---
+++ swap.py
@@ -0,0 +1,15 @@
+# Swap Two Numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Before swapping:")
+print("num1 =", num1)
+print("num2 =", num2)
+
+# Swapping
+num1, num2 = num2, num1
+
+print("After swapping:")
+print("num1 =", num1)
+print("num2 =", num2)
