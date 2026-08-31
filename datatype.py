95de515f3e669fc5286b265a5a43ae3c23f1fb9e
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:28:33 +0530

    datatype.py

---
+++ datatype.py
@@ -0,0 +1,17 @@
+# Understanding Data Types
+
+name = "Alex"          # String
+age = 12               # Integer
+height = 5.5           # Float
+is_student = True      # Boolean
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
+
+# Checking the data types
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
