8bfd1d768b42160ae7ca65d53ab9cdea2712654a
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:25:07 +0530

    variable.py

---
+++ variable.py
@@ -0,0 +1,11 @@
+# Creating variables
+name = "Amisha"
+age = 25
+height = 5.5
+is_student = True
+
+# Displaying the values
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
