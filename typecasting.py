a5f77b0f7b2927c7f6651abb70fe304efb58d78d
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:29:36 +0530

    typecasting.py

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+# Understanding Typecasting
+
+age = "12"
+price = "99.5"
+
+# Convert string to integer
+age = int(age)
+
+# Convert string to float
+price = float(price)
+
+print("Age:", age)
+print("Price:", price)
+
+# Convert number to string
+age_text = str(age)
+
+print("Age as text:", age_text)
+print(type(age_text))
