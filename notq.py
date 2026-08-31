b98949e74eac35f125bc1645e72be9b2619f4918
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:42:17 +0530

    note.py

---
+++ notq.py
@@ -0,0 +1,9 @@
+# Check if Two Numbers are Not Equal
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 != num2:
+    print("The numbers are not equal!")
+else:
+    print("The numbers are equal!")
