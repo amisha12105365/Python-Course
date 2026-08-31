6b33c427bf2936a5501f66c55a0bcfb837ba8c58
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:27:26 +0530

    storing_birthday.py

---
+++ storing_birthday.py
@@ -0,0 +1,12 @@
+# Storing Birthdays
+
+birthdays = {
+    "Alex": "15 March",
+    "Emma": "22 July",
+    "John": "10 December"
+}
+
+# Display birthdays
+print("Alex's birthday is:", birthdays["Alex"])
+print("Emma's birthday is:", birthdays["Emma"])
+print("John's birthday is:", birthdays["John"])
