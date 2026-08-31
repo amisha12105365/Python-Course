95235df955d067a033ec911b432965f6244e4e8b
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:45:33 +0530

    bitwise.py

---
+++ bitwise.py
@@ -0,0 +1,10 @@
+# Bitwise Operators
+
+a = 10
+b = 4
+
+print("AND (&):", a & b)
+print("OR (|):", a | b)
+print("XOR (^):", a ^ b)
+print("Left Shift (<<):", a << 1)
+print("Right Shift (>>):", a >> 1)
