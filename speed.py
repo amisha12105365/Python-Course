783fe90e8e1d3b12059ef889b9d94794245fea7d
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:50:11 +0530

    speed.py

---
+++ speed.py
@@ -0,0 +1,8 @@
+# Calculate Average Speed
+
+distance = float(input("Enter the distance in km: "))
+time = float(input("Enter the time in hours: "))
+
+average_speed = distance / time
+
+print("Average speed is:", average_speed, "km/h")
