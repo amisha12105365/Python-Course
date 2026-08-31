3809c40a330b934378d20f89ebd332ee1a35a8f3
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:26:17 +0530

    keywords.py

---
+++ keywords.py
@@ -0,0 +1,13 @@
+# Understanding Variables and Keywords
+
+name = "Alex"
+age = 12
+
+if age >= 10:
+    print("Name:", name)
+    print("Age:", age)
+else:
+    print("Age is less than 10")
+
+for i in range(3):
+    print("Welcome", name)
