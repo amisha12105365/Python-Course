def7aa4c078009d28bf8f270ac2b6da901fc6758
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:33:17 +0530

    count.py

---
+++ count.py
@@ -0,0 +1,9 @@
+# Count the Number of Notes
+
+notes = [10, 20, 10, 50, 20, 10, 100, 20]
+
+note = int(input("Enter the note value to count: "))
+
+count = notes.count(note)
+
+print("The note", note, "appears", count, "times.")
