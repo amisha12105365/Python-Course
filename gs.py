9681cba52240a147939b7aa32b2cd9398514d721
amisha12105365 <amisha12105365@gmail.com>
2026-09-01 02:38:38 +0530

    gs.py

---
+++ gs.py
@@ -0,0 +1,13 @@
+# Check Greater or Smaller
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+if num1 > num2:
+    print(num1, "is greater than", num2)
+
+elif num1 < num2:
+    print(num1, "is smaller than", num2)
+
+else:
+    print("Both numbers are equal!")
